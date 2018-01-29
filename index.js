8f7be1294763ec38ea28cfc65e48bed99924556f
Jafar-Draenor <school.work12@hotmail.com>
2018-01-29 13:20:26 +0000

    First commit

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const bot = new commando.Client({
 
 //Express
 var express = require('express');
-
 var app = express();
-
-app.set('view engine', 'html')
+app.set('view engine', 'ejs');
+var guildAmount = bot.guilds.size;
+var serverCount = {
+        "servercount": guildAmount
+}
+  
 
 app.use(express.static('public'))
 
-var guildAmount = bot.guilds.size;
-
-
 app.get('/', function(req, res){
   res.sendFile('public/index.html', { root : __dirname});
-  res.render('public/index.html', {guildAMount:guildAmount})
+  res.render('index',{user: "Great User",server:"2"});
 });
 
-console.log(bot.guilds.size)
 app.listen(process.env.PORT || 9000);
+console.log("Listening on port 9000")
+  
 
 //Declaring command groups and configuring where commands are stored (Discord.js-commando)
 bot.registry.registerGroup('random', 'Random');
@@ -30,9 +31,5 @@ bot.registry.registerGroup('wow', 'Wow')
 
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands"); //Directory for the command files
-
-
-//Server count GET request
-var theUrl = "https://wowghost.herokuapp.com"
 
 bot.login('MzMxMjQ4MzUyMDQ4MDU0Mjgy.DDsytw.hyEbsGU7y548RPQrTQ3M8_q3q-c'); //Login token for the bot
